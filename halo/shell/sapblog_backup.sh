e18e330c8af5ba3a7cc9b06bc18917aaca6df4fc
TINNQYOU <tinnkyou92@gmail.com>
2022-10-14 20:48:20 +0900

    5

---
+++ halo/shell/sapblog_backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+gitpath=/home/halo/github/sapblog-backup
+filename=sapblogs-$(date "+%Y-%m-%d-%H-%M-%S").tar.gz
+echo "[$(date "+%Y-%m-%d %H:%M:%S")] 开始备份博客数据"
+cd $gitpath
+tar -czvf $filename /home/halo/.halo/ > /dev/null &
+echo "[$(date "+%Y-%m-%d %H:%M:%S")] 数据压缩完成"
+echo "[$(date "+%Y-%m-%d %H:%M:%S")] 开始推送到Github"
+git add .
+git commit -m "[$(date "+%Y-%m-%d %H:%M:%S")] 博客数据备份"
+#git push origin master
+git push
+echo "[$(date "+%Y-%m-%d %H:%M:%S")] 推送到GitHub完成"
+
