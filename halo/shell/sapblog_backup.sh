3283ecb6e2c3e11c3de90848fd91bfa0300d454c
TINNQYOU <tinnkyou92@gmail.com>
2022-10-14 21:00:57 +0900

    [2022-10-14 21:00:57] 博客数据备份

--- halo/shell/sapblog_backup.sh
+++ halo/shell/sapblog_backup.sh
@@ -3,11 +3,13 @@ gitpath=/home/halo/github/sapblog-backup
 filename=sapblogs-$(date "+%Y-%m-%d-%H-%M-%S").tar.gz
 
 rsync -av --delete /home/halo/.halo/ /home/halo/github/sapblog-backup/halo/
-echo "[$(date "+%Y-%m-%d %H:%M:%S")] 开始备份博客数据"
-cd $gitpath
-tar -czvf $filename /home/halo/.halo/ > /dev/null &
-echo "[$(date "+%Y-%m-%d %H:%M:%S")] 数据压缩完成"
+echo "[$(date "+%Y-%m-%d %H:%M:%S")] 开始同步文件夹"
+rsync -av --delete /home/halo/.halo/ /home/halo/github/sapblog-backup/halo/ > /dev/null &
+#cd $gitpath
+#tar -czvf $filename /home/halo/.halo/ > /dev/null &
+echo "[$(date "+%Y-%m-%d %H:%M:%S")] 文件夹同步完成"
 echo "[$(date "+%Y-%m-%d %H:%M:%S")] 开始推送到Github"
+cd $gitpath
 git add .
 git commit -m "[$(date "+%Y-%m-%d %H:%M:%S")] 博客数据备份"
 #git push origin master
