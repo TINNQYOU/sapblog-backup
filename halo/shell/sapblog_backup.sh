4975de5598720c383fe5752ca61f575846274678
TINNQYOU <tinnkyou92@gmail.com>
2022-10-25 10:02:44 +0900

    [2022-10-25 10:02:44] 博客数据备份

--- halo/shell/sapblog_backup.sh
+++ halo/shell/sapblog_backup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 gitpath=/home/halo/github/sapblog-backup
+changeslog=/home/halo/logs/changeslistcount
+rsync -anv --delete /home/halo/.halo/ /home/halo/github/sapblog-backup/halo/ > $changeslog
+count=`wc -l $changeslog | awk '{print $1}'`
+if [ $count -eq 4 ] 
+then
+	echo "<==================没有修改的内容======================>"
+	exit 0
+fi
+
 echo "[$(date "+%Y-%m-%d %H:%M:%S")] 开始同步文件夹"
 rsync -av --delete /home/halo/.halo/ /home/halo/github/sapblog-backup/halo/ > /dev/null &
 echo "[$(date "+%Y-%m-%d %H:%M:%S")] 文件夹同步完成"
